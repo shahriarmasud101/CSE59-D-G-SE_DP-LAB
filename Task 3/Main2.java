3ac3b0da07bfe96441633ccd897cbd787b7442e5
shahriarmasud101 <cshahriar57@gmail.com>
2026-06-25 17:14:04 +0600

    Update Main2.java

--- Task 3/Main2.java
+++ Task 3/Main2.java
@@ -1,1 +1,49 @@
 
+public class Main2{
+    public static void main(String[] args) {
+        Shape[] shapes = {
+            new Rectangle(5,4),
+            new Circle(3)
+        };
+        AreaCalculator calculator = new AreaCalculator();
+        System.out.println("Total Area: "+ calculator.calculateTotalArea(shapes));
+    }
+}
+
+
+interface Shape {
+    double CalculateArea();
+}
+class Rectangle implements Shape{
+    private double length;
+    private double width;
+
+    public Rectangle(double length,double width){
+        this.length=length;
+        this.width=width;
+    }
+    public double CalculateArea(){
+        return length * width;
+    }
+}
+class Circle implements  Shape{
+    private double radius;
+
+    public Circle(double radius){
+        this.radius=radius;
+    }
+    public double CalculateArea(){
+        return Math.PI * radius * radius;
+
+    }
+}
+class AreaCalculator{
+    public double calculateTotalArea(Shape[] shapes){
+        double totalArea=0;
+
+        for ( Shape shape : shapes){
+            totalArea += shape.CalculateArea();
+        }
+        return totalArea;
+    }
+}
